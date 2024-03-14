5e6716e552f3f8acc29f10b5af02a9524812e0b5
singh <singhkailash2001@gmail.com>
2024-03-14 15:41:22 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,52 @@
+/*let i=0;
+
+while(i<=100){
+    console.log(i);
+    i++
+}*/
+//write a code printing half number of the given input
+/*let a=prompt("enter a number");
+let i=0;
+
+while(i<=0){
+    if(i==a/2){
+        console.log(i);
+        i++
+    }*/
+
+
+   // find the sum of odd numbers
+   /*let a = prompt("enter the numbrer");
+   let sum =0
+   let i =0
+    while(i<=a){
+    if(i%2 != 0){
+       sum = sum + i;
+    }   
+    i++;
+}
+console.log(sum);*/
+//find factorial number
+//fibonacci
+//perfect number
+ 
+/*
+let num=145;
+let fact=1;
+for(let j=i;j>=1;j--){
+    fact= fact*j
+}
+sum =sum + fact;
+num = parseInt(num/10);
+}
+console.log(sum)
+console.log(num)
+if(original num==sum){
+    console.log("strong number")
+}
+else{
+    console.log("not strong number")
+}
+
+
+
