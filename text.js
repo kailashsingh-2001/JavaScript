5f15ab52b63bf92bcf93243326b4773a356dfad8
singh <singhkailash2001@gmail.com>
2024-03-14 12:17:57 +0530

    variables const let var

--- text.js
+++ text.js
@@ -1,5 +1,15 @@
 console.log("kailash")
-console.log("singh")
-console.log("mango")
-console.log("intern")
-console.log("wipro")
+//variable and constant
+
+const accountId = 1223355 //it cannot be change
+let accountemail ="hhhh@gmail.com"//it can be changed  
+var  accountpassword = "1234"
+ accountcity = "jaipur"
+  accountId ="3333333"
+  accountemail = "jjjj@gmail.com"
+   accountpassword ="6789"
+   accountcity="pune"
+let accountstate;
+
+ console.log(accountId);
+ console.table([accountId, accountemail, accountpassword, accountcity , accountstate]);
