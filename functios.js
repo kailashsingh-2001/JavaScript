c4886ab4abfc79b7ecddcb862f784c66f331c66d
singh <singhkailash2001@gmail.com>
2024-03-15 14:42:25 +0530

    function types

--- functios.js
+++ functios.js
@@ -14,11 +14,26 @@ add(
     console.log(10,20)
 ) 
 //three types of function in js
-//nameing function
+//nameing function => 
 //anyanomous function
-//arrow function
+//arrow function 
 (function(){
     console.log("hello")
 })()
+
+
+
+
+
+/*
+what is clouser and why do we need thgis in javascript?
+what is function and why do we need function in programming/
+how many types of function in javascript?
+what is diffrence between naming functionn and anomoyous function ?
+what is diffrence between parameter and arguments?
+why we need return keyword in funtion?
+how many value return from one function?
+
+/*
 
 
