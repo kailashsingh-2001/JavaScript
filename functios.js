d99d114b54ce6afe1ac2c7ef90f80e910f3e4b64
singh <singhkailash2001@gmail.com>
2024-03-15 14:18:57 +0530

    function

---
+++ functios.js
@@ -0,0 +1,24 @@
+function add(a,b){
+    console.log(a+b)
+}
+function minus(a,b){
+    console.log(a-b)
+}
+function mul(a,b){
+    console.log(a*b)
+}
+function divide(a,b){
+    console.log(a/b)
+} 
+add(
+    console.log(10,20)
+) 
+//three types of function in js
+//nameing function
+//anyanomous function
+//arrow function
+(function(){
+    console.log("hello")
+})()
+
+
