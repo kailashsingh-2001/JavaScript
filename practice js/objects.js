7d667641121fb8cd6dc081ab8759a65959ec8540
singh <singhkailash2001@gmail.com>
2024-04-05 13:02:58 +0530

    js objects

---
+++ practice js/objects.js
@@ -0,0 +1,11 @@
+let obj1={
+    id:1,
+    name:'kailash',
+    age: 23,
+    email:'singkailash2001@gmail.com',
+    city:'jaipur'
+
+
+}
+console.log(obj1);
+console.log(obj1.age, obj1.id,obj1.email,obj1.city,obj1.email)
