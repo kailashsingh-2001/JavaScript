c51bf2d02baed942ca638c2e0241cc74b20eee1f
singh <singhkailash2001@gmail.com>
2024-04-05 13:38:24 +0530

    objects with array

--- practice js/objects.js
+++ practice js/objects.js
@@ -1,11 +1,34 @@
-let obj1={
-    id:1,
-    name:'kailash',
-    age: 23,
-    email:'singkailash2001@gmail.com',
-    city:'jaipur'
 
-
-}
-console.log(obj1);
-console.log(obj1.age, obj1.id,obj1.email,obj1.city,obj1.email)
+let obj1=[
+    {
+        id:1,
+        name:'kailash',
+        age: 23,
+        email:'singkailash2001@gmail.com',
+        city:'jaipur'
+    
+    
+    },
+    {
+        id:2,
+        name:'rakesh',
+        age: 26,
+        email:'rakesh@gmail.com',
+        city:'Nagaur'
+    },
+    {
+        id:3,
+        name:'mohan',
+        age: 25,
+        email:'mohan2001@gmail.com',
+        city:'delhi'
+    },
+    {
+    id:4,
+        name:'Rahu;',
+        age: 24,
+        email:'rahul2001@gmail.com',
+        city:'pune'
+    }
+    ]
+    console.log(obj1);
