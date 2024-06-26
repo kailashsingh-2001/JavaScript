e819d44e958f92f52e847641c45698776f42e0f9
singh <singhkailash2001@gmail.com>
2024-06-26 13:52:05 +0530

    24 june assingment

--- pratice javascript/24 june assignement/script.js
+++ pratice javascript/24 june assignement/script.js
@@ -1,4 +1,13 @@
 //1. aapko arrow function ke madad se 4 input lena hai aur uska sum calculate karna hai'
+
+var sum=(a,b,c,d)=>
+   {
+   return a+b+c+d;
+   }
+   sum(3,5,4,6);
+   
+    
+
 // 2. using annoymous arrow function create a function which does factorial of 5 
      const factorialof5 =(n=>{
         const factorial =num => num<=1 ? 1 :num * factorial(num-1);
@@ -6,5 +15,41 @@
      })(5)
      .console.log(factorialof5);
 // 3. use the concept of hoisiting and call a factorial function without declaring it and then define after calling
+   console.log(factorial(5)); 
+
+   function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+
 // 4. use the concept of immediately invoked function to check whether a number is even or odd. 
-// 5. using the concept of parameterised function constructor call factorial function
+
+var result =(function (a){
+  if(a%2 ===0 ){
+      console.log("even")
+  }
+  else{
+   console.log("odd")
+  }
+})(3);
+console.log(result);
+
+   
+// 5. u// Define a parameterized function constructor for calculating factorial
+const Factorial = new Function('n', `
+   if (n === 0 || n === 1) {
+       return 1;
+   } else {
+       return n * arguments.callee(n - 1);
+   }
+`);
+
+
+const result = Factorial(5); 
+console.log(result); 
+
+
